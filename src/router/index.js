3da0a97599b2aa16f70178caa7fcb6112fb31e7f
Daniil <andreevdaniil781@gmail.com>
2024-05-06 18:32:59 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const router = createRouter({
+  mode: "hash",
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {

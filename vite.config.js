7067caf33e260482ca11113335e68b56982ee060
Daniil <andreevdaniil781@gmail.com>
2024-06-05 12:52:32 +0300

    add metrika

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "@/assets/scss/main.scss";`
-      }
-    }
-  },
   plugins: [
     vue(),
     VueDevTools(),
